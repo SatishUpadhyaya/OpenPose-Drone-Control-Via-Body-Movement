0a00e7ac9e5d921a8ebd1f8db24304f1cdb845fc
satishupadhyaya <satishrajupadhyaya@gmail.com>
2020-05-03 21:49:51 -0600

    got keyboard controls with pygame done

--- init.py
+++ init.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python
 
 import rospy
-from mavros_msgs.srv import SetMode
-from mavros_msgs.srv import CommandBool
-from mavros_msgs.srv import CommandTOL
+from mavros_msgs.srv import SetMode, CommandBool, CommandTOL
+from geometry_msgs.msg import TwistStamped
 import time
+import pygame, sys
+import pygame.locals
 
 rospy.init_node('mavros_final_project')
 rate = rospy.Rate(10)
+
+commandVelocityPub = rospy.Publisher('/mavros/setpoint_velocity/cmd_vel', TwistStamped, queue_size = 10)
 
 def setMode():
     print("Setting Mode to Guided: mode guided")
@@ -31,7 +34,7 @@ def takeOff():
     print("Taking off: takeoff 10")
     try:
         mavTakeOff = rospy.ServiceProxy('/mavros/cmd/takeoff', CommandTOL)
-        mavTakeOff(altitude=10)
+        mavTakeOff(altitude=1)
     except rospy.ServiceException as e:
         print(e)
 
@@ -39,7 +42,7 @@ def land():
     print("Landing: mode land")
     try:
         mavLand = rospy.ServiceProxy('/mavros/cmd/land', CommandTOL)
-        mavLand(altitude=10)
+        mavLand(altitude=1)
     except rospy.ServiceException as e:
         print(e)
 
@@ -51,11 +54,90 @@ def disarmCopter():
     except rospy.ServiceException as e:
         print(e)
 
+def moveLeft():
+    print("Move Left:")
+    setVelocity = TwistStamped()
+    setVelocity.twist.linear.x = -0.5
+    commandVelocityPub.publish(setVelocity)
+
+def moveRight():
+    print("Move Right:")
+    setVelocity = TwistStamped()
+    setVelocity.twist.linear.x = 0.5
+    commandVelocityPub.publish(setVelocity)
+
+def moveForward():
+    print("Move Forward:")
+    setVelocity = TwistStamped()
+    setVelocity.twist.linear.y = 0.5
+    commandVelocityPub.publish(setVelocity)
+
+def moveBackward():
+    print("Move Back:")
+    setVelocity = TwistStamped()
+    setVelocity.twist.linear.y = -0.5
+    commandVelocityPub.publish(setVelocity)
+
+def moveUp():
+    print("Move Up:")
+    setVelocity = TwistStamped()
+    setVelocity.twist.linear.z = 0.5
+    commandVelocityPub.publish(setVelocity)
+
+def moveDown():
+    print("Move Down:")
+    setVelocity = TwistStamped()
+    setVelocity.twist.linear.z = -0.5
+    commandVelocityPub.publish(setVelocity)
+
+def rotateRight():
+    print("Rotate Right:")
+    setVelocity = TwistStamped()
+    setVelocity.twist.angular.z = 0.5
+    commandVelocityPub.publish(setVelocity)
+
+def rotateLeft():
+    print("Rotate Left:")
+    setVelocity = TwistStamped()
+    setVelocity.twist.angular.z = -0.5
+    commandVelocityPub.publish(setVelocity)
+
 if __name__=="__main__":
-    setMode()
-    armCopter()
-    takeOff()
-    time.sleep(10) # hover for 10 ms
-    land()
-    disarmCopter()
-    print("Done")
+    print("MAIN FUNCTION")
+    pygame.init()
+    BLACK = (0, 0, 0)
+    WIDTH = (500)
+    HEIGHT = (500)
+    windowSurface = pygame.display.set_mode((WIDTH, HEIGHT), 0, 32)
+    windowSurface.fill(BLACK)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_t:
+                    setMode()
+                    armCopter()
+                    takeOff()
+                    time.sleep(5)
+                if event.key == pygame.K_l:
+                    land()
+                    disarmCopter()
+                    time.sleep(5)
+                if event.key == pygame.K_w:
+                    moveForward()
+                if event.key == pygame.K_a:
+                    moveLeft()
+                if event.key == pygame.K_d:
+                    moveRight()
+                if event.key == pygame.K_s:
+                    moveBackward()
+                if event.key == pygame.K_q:
+                    moveUp()
+                if event.key == pygame.K_e:
+                    moveDown()
+                if event.key == pygame.K_z:
+                    rotateLeft()
+                if event.key == pygame.K_x:
+                    rotateRight()
+            elif event.type == pygame.QUIT:
+                quit()
