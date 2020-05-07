46b44198655f6164654a46068ea5f61486f3a0ab
satishupadhyaya <satishrajupadhyaya@gmail.com>
2020-05-06 21:05:31 -0600

    added the angular.z rotation

--- arucoControl.py
+++ arucoControl.py
@@ -10,12 +10,39 @@ from keyboardControl import setMode, armCopter, takeOff, land, disarmCopter, com
 import time
 
 # this arcuo ID can be found at: http://chev.me/arucogen
+# Helpful video on the topic: https://www.youtube.com/watch?v=wlT_0fhGrGg
 arucoID = 72 # ID: 72
 markerSize = 10 # 100 mm
 cameraMatrix = np.loadtxt('cameraMatrix.txt', delimiter=',')
 cameraDistortion = np.loadtxt('cameraDistortion.txt', delimiter=',')
 arucoDict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_ARUCO_ORIGINAL) # aruco auto dict
 parameters = cv2.aruco.DetectorParameters_create()
+
+RotationMatrix = np.matrix([[1., 0., 0.], [0., -1., 0.], [0., 0., -1.]])
+
+# Code for Rotation matrix to Euler Angles
+# from https://www.learnopencv.com/rotation-matrix-to-euler-angles/
+def isRotationMatrix(R):
+    Rt = np.transpose(R)
+    shouldBeIdentity = np.dot(Rt, R)
+    I = np.identity(3, dtype=R.dtype)
+    n = np.linalg.norm(I - shouldBeIdentity)
+    return n < 1e-6
+
+def rotationMatrixToEulerAngles(R):
+    assert (isRotationMatrix(R))
+    sy = math.sqrt(R[0, 0] * R[0, 0] + R[1, 0] * R[1, 0])
+    singular = sy < 1e-6
+    if not singular:
+        x = math.atan2(R[2, 1], R[2, 2])
+        y = math.atan2(-R[2, 0], sy)
+        z = math.atan2(R[1, 0], R[0, 0])
+    else:
+        x = math.atan2(-R[1, 2], R[1, 1])
+        y = math.atan2(-R[2, 0], sy)
+        z = 0
+
+    return np.array([x, y, z])
 
 
 if __name__=="__main__":
@@ -48,13 +75,16 @@ if __name__=="__main__":
             cv2.aruco.drawDetectedMarkers(frame, corners)
             cv2.aruco.drawAxis(frame, cameraMatrix, cameraDistortion, rvec, tvec, 10)
 
-            myString = f"Aruco: x={x}  y={y}  z={z}"
+            _, _, yaw = rotationMatrixToEulerAngles(RotationMatrix*np.matrix(cv2.Rodrigues(rvec)[0]).T)
+            yaw = math.degrees(yaw)
+
+            myString = f"Aruco: x={int(x)}  y={int(y)}  z={int(z)} yaw = {int(yaw)}"
             cv2.putText(frame, myString, (0, 100), font, 1, (255, 255, 255), 2, cv2.LINE_AA)
 
             if x > 10:
                 # move left
                 setVelocity.twist.linear.x = -0.5
-            if x < -10:
+            if x < -15:
                 # move right
                 setVelocity.twist.linear.x = 0.5
             if y < -8:
@@ -63,12 +93,18 @@ if __name__=="__main__":
             if y > 6:
                 # move down
                 setVelocity.twist.linear.z = -0.5
-            if z < 27:
+            if z < 30:
                 # move forward
                 setVelocity.twist.linear.y = 0.5
             if z > 50:
                 # move backward
                 setVelocity.twist.linear.y = -0.5
+            if yaw > 25:
+                # rotate right
+                setVelocity.twist.angular.z = -0.5
+            if yaw < -25:
+                # rotate left
+                setVelocity.twist.angular.z = 0.5
             
             # publishing the TwistStamped message    
             commandVelocityPub.publish(setVelocity)
